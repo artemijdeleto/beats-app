45679421e85858829d812e5a23cb79df7955d969
Artemij <artemijdeleto@gmail.com>
2019-10-19 14:55:32 +0400

    Add english translation for menu

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,3 +1,16 @@
 export default {
+	menu: {
+		login: 'Log in',
+		upload: 'Upload',
 
+		dashboard: 'Dashboard',
+		orders: 'Orders',
+		stats: 'Statistics',
+		settings: 'Settings',
+
+		home: 'Home',
+		search: 'Search',
+		music: 'Music',
+		artists: 'Artists'
+	}
 }
