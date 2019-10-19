6a1dcde08f0a94722273a12575cea16d134bc38b
Artemij <artemijdeleto@gmail.com>
2019-10-19 14:16:53 +0400

    Add dynamic import for locales

--- src/i18n.js
+++ src/i18n.js
@@ -4,8 +4,30 @@ import en from '@/locales/en'
 
 Vue.use(VueI18n)
 
-export default new VueI18n({
+const cache = ['en'];
+const i18n = new VueI18n({
 	locale: 'en',
 	fallbackLocale: 'en',
 	messages: { en }
 });
+
+function apply(locale) {
+	i18n.locale = locale;
+	return locale;
+}
+
+export function setLocale(locale) {
+	if (i18n.locale === locale || cache.includes(locale)) {
+		return Promise.resolve(apply(locale));
+	}
+
+	return import(/* webpackChunkName: "locale-[request]" */ `@/locales/${locale}`).then(
+		messages => {
+			i18n.setLocaleMessage(locale, messages.default);
+			cache.push(locale);
+			return apply(locale);
+		}
+	)
+}
+
+export default i18n;
