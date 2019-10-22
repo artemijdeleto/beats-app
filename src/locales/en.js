2afd414115c99e11efc13cb9fa43589e723c36ff
Artemij <artemijdeleto@gmail.com>
2019-10-22 18:42:42 +0400

    Add translation for login view

--- src/locales/en.js
+++ src/locales/en.js
@@ -12,5 +12,15 @@ export default {
 		search: 'Search',
 		music: 'Music',
 		artists: 'Artists'
+	},
+	sign: {
+		title: 'Authorization',
+		username: 'Username',
+		nickname: 'Nickname',
+		password: 'Password',
+		login: 'Log in',
+		register: 'Register',
+		create: 'Create account',
+		registered: 'I\'ve already registered'
 	}
 }
--- src/views/AccountSign.vue
+++ src/views/AccountSign.vue
@@ -1,25 +1,25 @@
 <template>
 	<div>
-		<h1>Авторизация</h1>
+		<h1>{{ $t('sign.title') }}</h1>
 		<form @submit.prevent="submit()">
 
 			<label class="form-group">
-				<span>Логин</span>
+				<span>{{ $t('sign.username') }}</span>
 				<input v-model="username" class="form-control">
 			</label>
 			<br>
 			<label v-if="!login" class="form-group">
-				<span>Псевдоним</span>
+				<span>{{ $t('sign.nickname') }}</span>
 				<input v-model="nickname" class="form-control">
 			</label>
 			<br>
 			<label class="form-group">
-				<span>Пароль</span>
+				<span>{{ $t('sign.password') }}</span>
 				<input v-model="password" class="form-control" :type="login ? 'password' : 'text'">
 			</label>
 			<br><br>
-			<button class="button button--large button--accent mr-1">{{ login ? 'Войти' : 'Зарегистрироваться' }}</button>
-			<button class="button" type="button" @click="login = !login">{{ login ? 'Создать аккаунт' : 'Я уже зарегистрирван' }}</button>
+			<button class="button button--large button--accent mr-1">{{ login ? $t('sign.login') : $t('sign.register') }}</button>
+			<button class="button" type="button" @click="login = !login">{{ login ? $t('sign.create') : $t('sign.registered') }}</button>
 		</form>
 	</div>
 </template>
